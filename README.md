d98c315d4083a9e0457348ed7f9b5e216c7348dd
Tanner Anderson <me@tanner.technology>
2020-02-24 21:54:52 -0700

    v1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # GTM-for-YOURLS
 Google Tag Manager for every single click
+
+# Use
+Simply open plugin.php and replace GTM-XXXXXX1 with your tagid
+
+Copy to your plugins folder
---
+++ gtm-for-yourls/plugin.php
@@ -0,0 +1,39 @@
+<?php
+/*
+Plugin Name: GoogleTagManager for YOURLS
+Plugin URI: https://tanner.technology/GTM
+Description: Adds google tag manager tracking to every clicked link
+Version: 1
+Author: Tanner Anderson
+Author URI: https://tanner.technology/
+*/
+
+if( !defined( 'YOURLS_ABSPATH' ) ) die();
+
+yourls_add_action( 'pre_redirect', 'gtm_add_tracking' );
+
+function gtm_add_tracking( $args ) {
+    $url = $args[0];
+    $code = $args[1];
+    $tagid = "GTM-XXXXXX1"; // SET TAGID HERE
+
+    echo "<html>";
+    echo "<head>";
+    echo "<!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','$tagid');</script>
+    <!-- End Google Tag Manager -->";
+    echo "</head>";
+
+    echo "<body>";
+    echo "<!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src=\"https://www.googletagmanager.com/ns.html?id=$tagid\"
+    height=\"0\" width=\"0\" style=\"display:none;visibility:hidden\"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->";
+    echo "<p>Please <a href='$url'>click here</a> if you are not automatically redirected.</p>";
+    echo "</body>";
+    echo "</html>";
+}
